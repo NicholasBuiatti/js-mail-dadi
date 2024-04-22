789ca41f582d886ce96f25f1fe2120b642d4c927
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-22 19:31:03 +0200

    Mi fermo qua

--- js/mail.js
+++ js/mail.js
@@ -1,15 +1,38 @@
-const User = prompt(`Digita il tuo nome`);
-
 const listUser = [`Pippo`, `Pluto`, `Paperino`, `Sora`, `Rikku`];
 console.log(listUser);
 
-// const signIn = prompt('Resgistrati');
+// let User = prompt()
 
-for (i = 0; i < listUser.length; i++) {
-    if (User === listUser[i]) {
-        i = User
-        console.log(`Puoi entrare`);
-    } else {
-        console.log('Non puoi');
+// if (listUser.includes(User)) {
+//     console.log('accesso consentito');
+// } else {
+//     console.log('accesso negato');
+// }
+
+const mybtn = document.getElementById('mybtn');
+
+mybtn.addEventListener('click', function () {
+    const User = document.querySelector('input').value;
+    for (i = 0; i < listUser.length; i++) {
+        if (User === listUser[i]) {
+            i = User;
+            console.log(`Puoi entrare`);
+            document.querySelector('form').innerHTML = `<div>Accettato</div>`
+        } else {
+            console.log('Non puoi');
+            document.querySelector('form').innerHTML = `<div>Negato</div>`
+        }
     }
-}
+})
+
+const mybtn2 = document.getElementById('mybtn2');
+
+mybtn2.addEventListener('click', function () {
+    const UserRegistrazione = document.getElementById('registrazione').value;
+    listUser.push(UserRegistrazione);
+    console.log(listUser);
+})
+
+
+
+
